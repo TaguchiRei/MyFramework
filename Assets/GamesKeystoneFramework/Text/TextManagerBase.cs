d5e611d253fac9848b46b7a88fab8747b7275425
TaguchiRei <ren.len2426@gmail.com>
2025-02-26 22:11:22 +0900

    TypeTextメソッド完成

--- Assets/GamesKeystoneFramework/Text/TextManagerBase.cs
+++ Assets/GamesKeystoneFramework/Text/TextManagerBase.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 using System.Text.RegularExpressions;
 using GamesKeystoneFramework.Core.Text;
 using TMPro;
@@ -32,7 +34,8 @@ namespace GamesKeystoneFramework.Text
         /// </summary>
         private int _lineNumber = 0;
         private int _questionIndentation = 0;
-        private int _readPoint = 0;
+        private bool _movingCoroutin;
+        private bool _selectMode = false;
         
         
         private void Start()
@@ -46,6 +49,7 @@ namespace GamesKeystoneFramework.Text
 
         public virtual void TextStart(TextDataScriptable textDataScriptable, int selectionIndex, Action action = null)
         {
+            _lineNumber = 0;
             action?.Invoke();
             TextBox(true);
             _dataList = TextUpdate(textDataScriptable.TextDataList[selectionIndex].DataList); 
@@ -54,28 +58,91 @@ namespace GamesKeystoneFramework.Text
 
         void Next()
         {
-            switch (_dataList[_lineNumber].DataType)
+            if (_movingCoroutin)
             {
-                case TextDataType.Text:
-                    break;
-                case TextDataType.Question:
-                    break;
-                case TextDataType.Branch:
-                    break;
-                case TextDataType.QEnd:
-                    break;
-                case TextDataType.TextEnd:
-                    break;
-                default:
-                    break;
+                
+            }
+            else
+            {
+                if (_selectMode)
+                {
+                    
+                }
+                else
+                {
+                    //次のテキストを読み込んで適切な処理をする
+                    switch (_dataList[_lineNumber].DataType)
+                    {
+                        case TextDataType.Text:
+                            StartCoroutine(TypeText(_dataList[_lineNumber].Text));
+                            break;
+                        case TextDataType.Question:
+                            _selectMode = true;
+                            _questionIndentation++;
+                            StartCoroutine(TypeText(_dataList[_lineNumber].Text, (() => SelectBox(true))));
+                            break;
+                        case TextDataType.Branch:
+                            
+                            break;
+                        case TextDataType.QEnd:
+                            break;
+                        case TextDataType.TextEnd:
+                            break;
+                        default:
+                            break;
+                    }
+                }
             }
         }
 
-        void TextWrite()
+        /// <summary>
+        /// テキストを表示するコルーチン。
+        /// </summary>
+        /// <param name="text">表示したいテキスト</param>
+        /// <param name="action">テキスト表示後に実行される</param>
+        /// <returns></returns>
+        IEnumerator TypeText(string text,Action action = null)
         {
+            _movingCoroutin = true;
+            //何行あるかを調べ、新規で行が追加されることを念頭に基底行数より多ければ一行目を削除して繰り上げする。
+            var s = mainText.text.Split('\n').ToList();
+            var sb = new StringBuilder();
+            if (s.Count + 1 > line)
+            {
+                s.RemoveAt(0);
+                sb.Append(string.Join("\n", s));
+            }
+            mainText.maxVisibleCharacters = mainText.GetParsedText().Length;
+            sb.Append("\n");
+            sb.Append(text);
+            mainText.text = sb.ToString();
+           
+            if (displayCharOneByOne)
+            {
+                //一文字づつ表示
+                for (int i = 0; i < text.Length; i++)
+                {
+                    mainText.maxVisibleCharacters++;
+                    yield return new WaitForSeconds(writeSpeed);
+                }
+            }
+            else
+            {
+                //一気に表示
+                mainText.maxVisibleCharacters = mainText.GetParsedText().Length;
+            }
             
+            //完了後の処理
+            _lineNumber++;
+            action?.Invoke();
+            _movingCoroutin = false;
         }
         
+        /// <summary>
+        /// テキストの中の名前を設定する
+        /// </summary>
+        /// <param name="dataList"></param>
+        /// <returns></returns>
         private List<TextData> TextUpdate(List<TextData> dataList)
         {
             const string pattern = @"/name(\d)";
