fd0ab1398cdda93776dce763effee71fac66fd3a
TaguchiRei <ren.len2426@gmail.com>
2025-03-08 14:53:36 +0900

    非同期でセーブを実行できるように

--- Assets/GamesKeystoneFramework/Save/SaveDataBase.cs
+++ Assets/GamesKeystoneFramework/Save/SaveDataBase.cs
@@ -1,6 +1,7 @@
 using System;
 using System.IO;
 using UnityEngine;
+using Cysharp.Threading.Tasks;
 
 namespace GamesKeystoneFramework.Save
 {
@@ -15,25 +16,16 @@ namespace GamesKeystoneFramework.Save
         /// </summary>
         /// <param name="dataNumber"></param>
         /// <param name="fileName"></param>
-        public void Save(int dataNumber, string fileName = "SaveData")
+        public async UniTask Save(int dataNumber, string fileName = "SaveData")
         {
             string path = Application.persistentDataPath + $"/{fileName + dataNumber}.json";
-            if (File.Exists(path))
-            {
 #if UNITY_EDITOR
-                Debug.Log("File Exists");
+            Debug.Log(File.Exists(path) ? "File Exists" : "File Not Exists");
 #endif
-            }
-            else
-            {
-#if UNITY_EDITOR
-                Debug.Log("File Not Exists");
-#endif
-            }
-            File.WriteAllText(path, JsonUtility.ToJson(this));
-            
+            await  File.WriteAllTextAsync(path, JsonUtility.ToJson(this));
         }
-        public T Load(int dataNumber, string fileName = "SaveData")
+        
+        public async UniTask<T> Load(int dataNumber, string fileName = "SaveData")
         {
             string path = Application.persistentDataPath + $"/{fileName + dataNumber}.json";
             if (File.Exists(path))
@@ -41,15 +33,13 @@ namespace GamesKeystoneFramework.Save
 #if UNITY_EDITOR
                 Debug.Log("File Exists");
 #endif
-                return JsonUtility.FromJson<T>(File.ReadAllText(path));
+                var json = await File.ReadAllTextAsync(path);
+                return JsonUtility.FromJson<T>(json);
             }
-            else
-            {
 #if UNITY_EDITOR
-                Debug.Log("File Not Exists");
+            Debug.Log("File Not Exists");
 #endif
-                return default;
-            }
+            return default;
         }
         /// <summary>
         /// セーブデータの初期化を行う
