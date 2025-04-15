0845631629d38f03afe5e764142e0b427e96926e
TaguchiRei <ren.len2426@gmail.com>
2025-04-15 23:30:59 +0900

    [Add]ロビー参加メソッドが完成

--- Assets/GamesKeystoneFramework/MultiPlaySystem/MUltiPlayClientSystem.cs
+++ Assets/GamesKeystoneFramework/MultiPlaySystem/MUltiPlayClientSystem.cs
@@ -1,16 +1,21 @@
+using System;
+using Cysharp.Threading.Tasks;
+using Unity.Services.Lobbies;
 using UnityEngine;
 
 public class MUltiPlayClientSystem : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private async UniTask<bool> JoinLobby(string lobbyCode)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        try
+        {
+            await LobbyService.Instance.JoinLobbyByCodeAsync("LobbyCode");
+            return true;
+        }
+        catch(Exception e)
+        {
+            Debug.LogError($"Join Lobby Error{e.Message}");
+            return false;
+        }
     }
 }
