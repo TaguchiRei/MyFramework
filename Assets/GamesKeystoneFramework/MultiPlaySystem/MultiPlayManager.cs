4e00cdfd2c255e31c9813d5035d205f451cf409e
TaguchiRei <ren.len2426@gmail.com>
2025-04-15 20:44:03 +0900

    [Add]ホスティングする際にロビーを作成するためのメソッド作成

--- Assets/GamesKeystoneFramework/MultiPlaySystem/MultiPlayManager.cs
+++ Assets/GamesKeystoneFramework/MultiPlaySystem/MultiPlayManager.cs
@@ -1,21 +1,35 @@
 using System;
 using Cysharp.Threading.Tasks;
+using Unity.Netcode;
+using Unity.Netcode.Transports.UTP;
+using Unity.Networking.Transport.Relay;
 using Unity.Services.Authentication;
 using Unity.Services.Core;
+using Unity.Services.Lobbies;
+using Unity.Services.Relay;
+using Unity.Services.Relay.Models;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Networking.PlayerConnection;
 
 public class MultiPlayManager : MonoBehaviour
 {
+    [Header("初期化完了しているかどうか")]
     public bool canMultiPlay;
 
-
-    private void Awake()
-    {
-        canMultiPlay = false;
-    }
+    [Header("マルチプレイ参加可能人数")]
+    [SerializeField] private int _numberObParticipants;
+    
+    private CreateLobbyOptions createLobbyOptions;
     
     /// <summary>
-    /// ホストする際にサービスを初期化する
+    /// ロビーのリスト
+    /// </summary>
+    private QueryLobbiesOptions queryLobbiesOptions;
+
+    /// <summary>
+    /// ホストする際にサービスを初期化する。
+    /// 一度だけ使用
     /// </summary>
     private async UniTask ServicesInitialize()
     {
@@ -26,5 +40,20 @@ public class MultiPlayManager : MonoBehaviour
             //アカウントが無かった場合新たに作成
             await AuthenticationService.Instance.SignInAnonymouslyAsync();
         }
+        Debug.Log("Services initialized");
+    }
+
+    /// <summary>
+    /// ロビーを作成する際に使用するメソッド。要改善
+    /// </summary>
+    private async UniTask CreateLobby()
+    {
+        var allocation = await RelayService.Instance.CreateAllocationAsync(_numberObParticipants);
+
+        var relayServerData = allocation.ToRelayServerData("dtls");
+        NetworkManager.Singleton.GetComponent<UnityTransport>()
+            .SetRelayServerData(new RelayServerData());
+        NetworkManager.Singleton.GetComponent<UnityTransport>().SetRelayServerData(relayServerData);
+        NetworkManager.Singleton.StartHost();
     }
 }
