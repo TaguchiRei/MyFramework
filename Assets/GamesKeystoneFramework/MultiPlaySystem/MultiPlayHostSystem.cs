4ea8d5638c6959e1ef226408c03fcca699ef83c7
TaguchiRei <ren.len2426@gmail.com>
2025-04-15 21:42:12 +0900

    [Add]ロビー作成用プログラムをMultiPlayHostSystemに移動、通信エラー等発生時のエラーハンドリング追加

--- Assets/GamesKeystoneFramework/MultiPlaySystem/MultiPlayHostSystem.cs
+++ Assets/GamesKeystoneFramework/MultiPlaySystem/MultiPlayHostSystem.cs
@@ -1,9 +1,35 @@
+using System;
 using Cysharp.Threading.Tasks;
-using Unity.Services.Authentication;
-using Unity.Services.Core;
+using Unity.Netcode;
+using Unity.Netcode.Transports.UTP;
+using Unity.Services.Relay;
+using Unity.Services.Relay.Models;
 using UnityEngine;
 
 public class MultiPlayHostSystem : MonoBehaviour
 {
-    
+    [Header("マルチプレイ参加可能人数")]
+    [SerializeField] private int _numberObParticipants;
+
+    private string joinCode;
+    /// <summary>
+    /// ロビーを作成する際に使用するメソッド。要改善
+    /// </summary>
+    private async UniTask<bool> CreateLobby()
+    {
+        try
+        {
+            var allocation = await RelayService.Instance.CreateAllocationAsync(_numberObParticipants);
+            var relayServerData = allocation.ToRelayServerData("dtls");
+            joinCode = await RelayService.Instance.GetJoinCodeAsync(allocation.AllocationId);
+            NetworkManager.Singleton.GetComponent<UnityTransport>().SetRelayServerData(relayServerData);
+            NetworkManager.Singleton.StartHost();
+            return true;
+        }
+        catch (Exception e)
+        {
+            Debug.LogError(e);
+            return false;
+        }
+    }
 }
