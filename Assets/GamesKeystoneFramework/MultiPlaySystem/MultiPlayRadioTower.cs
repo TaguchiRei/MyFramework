164869ede238110e5e5899a9f9c2a42102fab470
TaguchiRei <ren.len2426@gmail.com>
2025-04-20 02:48:25 +0900

    [Add]データの送受信をするスクリプト作成開始

---
+++ Assets/GamesKeystoneFramework/MultiPlaySystem/MultiPlayRadioTower.cs
@@ -0,0 +1,24 @@
+using Unity.Netcode;
+using UnityEngine;
+
+namespace GamesKeystoneFramework.MultiPlaySystem
+{
+    public class MultiPlayRadioTower : NetworkBehaviour
+    {
+        /// <summary>
+        /// クライアントにデータを送信
+        /// </summary>
+        public void SendDataToClient()
+        {
+            
+        }
+
+        /// <summary>
+        /// サーバーにデータを送信
+        /// </summary>
+        public void SendDataToServer()
+        {
+            
+        }
+    }
+}
