457c37562115550fbf9570a3bec3e66afacd2d09
TaguchiRei <ren.len2426@gmail.com>
2025-04-20 00:40:52 +0900

    [Add]通信切断メソッドを追加

--- Assets/GamesKeystoneFramework/MultiPlaySystem/MultiPlayManagerBase.cs
+++ Assets/GamesKeystoneFramework/MultiPlaySystem/MultiPlayManagerBase.cs
@@ -1,26 +1,32 @@
 using System;
+using System.Collections.Generic;
 using Cysharp.Threading.Tasks;
+using GamesKeystoneFramework.Attributes;
 using Unity.Mathematics;
 using Unity.Netcode;
 using Unity.Services.Authentication;
 using Unity.Services.Core;
 using Unity.Services.Lobbies;
+using Unity.Services.Lobbies.Models;
 using UnityEngine;
 
 namespace GamesKeystoneFramework.MultiPlaySystem
 {
-    public class MultiPlayManager : MonoBehaviour
+    public abstract class MultiPlayManager : MonoBehaviour
     {
         [SerializeField] private GameObject MultiPlayObjectGroup;
         [Header("初期化完了しているかどうか")]
         public static bool CanMultiPlay;
+        
+        [SerializeField, ReadOnlyInInspector] LobbyData lobbyData;
     
         /// <summary>
         /// ロビーのリスト
         /// </summary>
         public static QueryLobbiesOptions QueryLobbiesOptions;
-    
-    
+
+        private Lobby _joinedLobby;
+        
         /// <summary>
         /// ホストする際にサービスを初期化する。
         /// 一度だけ使用する
@@ -50,6 +56,33 @@ namespace GamesKeystoneFramework.MultiPlaySystem
             return true;
         }
 
+        /// <summary>
+        /// 通信を切断する際に使用する
+        /// </summary>
+        /// <returns></returns>
+        public async UniTask<bool> DisConnect()
+        {
+            if(!NetworkManager.Singleton.IsHost && !NetworkManager.Singleton.IsServer)return false;
+            try
+            {
+                NetworkManager.Singleton.Shutdown();
+                await LobbyService.Instance.RemovePlayerAsync(_joinedLobby.Id, AuthenticationService.Instance.PlayerId);
+                return true;
+            }
+            catch (Exception e)
+            {
+                Debug.LogError($"Disconnect Error : {e.Message}");
+                return false;
+            }
+        }
+
+        public async UniTask<bool> HostConnect()
+        {
+            if(!CanMultiPlay) return false;
+            
+            var 
+        }
+
         public GameObject InstantiateMultiObject(GameObject obj,Vector3 position)
         {
             if (obj == null)
@@ -73,5 +106,20 @@ namespace GamesKeystoneFramework.MultiPlaySystem
 
             return spawnObj;
         }
+        
+        
+    }
+    /// <summary>
+    /// ロビー作成時に設定する項目をまとめた構造体
+    /// </summary>
+    [Serializable]
+    public struct LobbyData
+    {
+        public string LobbyName;
+        public int MaxPlayers;
+        public bool IsPrivate;
+        public bool IsLocked;
+        public DataObject.VisibilityOptions VisibilityOptions;
+        public Dictionary<string, DataObject> Data;
     }
 }
