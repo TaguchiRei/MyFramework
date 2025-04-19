ccec316a7e3722d78b015d8f3f1f83e310129f1b
TaguchiRei <ren.len2426@gmail.com>
2025-04-19 19:50:50 +0900

    [Update]正しい名前空間に移動

--- Assets/GamesKeystoneFramework/Editor/InspectorSupport.cs
+++ Assets/GamesKeystoneFramework/Editor/InspectorSupport.cs
@@ -1,17 +1,18 @@
-using System.Linq;
-using System.Reflection;
 using UnityEditor;
 using UnityEngine;
 
-[CustomEditor(typeof(MonoBehaviour), true)]
-public class InspectorSupport : Editor
+namespace GamesKeystoneFramework.Editor
 {
-    private void OnEnable()
+    [CustomEditor(typeof(MonoBehaviour), true)]
+    public class InspectorSupport : UnityEditor.Editor
     {
+        private void OnEnable()
+        {
 
-    }
-    public override void OnInspectorGUI()
-    {
-        base.OnInspectorGUI();
+        }
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+        }
     }
 }
