70bf890112949eec6edf68a7d2cecf0219877b63
TaguchiRei <ren.len2426@gmail.com>
2025-05-18 17:32:14 +0900

    [Add]整理用スクリプタブルオブジェクト、整理用スクリプト作成開始

---
+++ Assets/GamesKeystoneFramework/Organize/KeyOrganizeObject.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace GamesKeystoneFramework.Organize
+{
+    public class KeyOrganizeObject : MonoBehaviour
+    {
+        
+    }
+}
---
+++ Assets/GamesKeystoneFramework/Scriptable/KeyTags.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace GamesKeystoneFramework.Scriptable
+{
+    [CreateAssetMenu(fileName = "KeyTags", menuName = "Scriptable Objects/KeyTags")]
+    public class KeyTags : ScriptableObject
+    {
+        
+    }
+}
