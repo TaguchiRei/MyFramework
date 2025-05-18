b46bbd6b0b90264c18e3a40e3678a8febd786231
TaguchiRei <ren.len2426@gmail.com>
2025-05-19 04:45:55 +0900

    [Add]変更を監視するためのクラスを作成。

---
+++ Assets/GamesKeystoneFramework/Test/KeyVariablesObserver.cs
@@ -0,0 +1,43 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+namespace GamesKeystoneFramework.Test
+{
+    public class KeyObservable<T>
+    {
+        private T _value;
+
+#if UNITY_EDITOR
+        private T _oldValue;
+#endif
+
+        public T Value
+        {
+            get => _value;
+            set
+            {
+                _value = value;
+#if UNITY_EDITOR
+                if (_value != null && _oldValue != null)
+                {
+                    if (!_value.Equals(_oldValue))
+                    {
+                        Debug.Log($"(\u25c9ω\u25c9\uff40)<value changed… : {_oldValue} → {_value}");
+                        _oldValue = _value;
+                    }
+                }
+                else
+                {
+                    _oldValue = _value;
+                }
+#endif
+            }
+        }
+
+        public KeyObservable(T initialValue = default)
+        {
+            _value = initialValue;
+            _oldValue = initialValue;
+        }
+    }
+}
