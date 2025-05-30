ba75f2aabbf47e47e6ad3a58317c887db690d6c9
TaguchiRei <ren.len2426@gmail.com>
2025-05-30 12:29:51 +0900

    [update]stringBuilderによる文章結合を廃止

--- Assets/GamesKeystoneFramework/KeyDebug/KeyTester.cs
+++ Assets/GamesKeystoneFramework/KeyDebug/KeyTester.cs
@@ -1,18 +1,20 @@
+using System.Diagnostics;
 using System.Text;
 using UnityEngine;
+using Debug = UnityEngine.Debug;
 
 namespace GamesKeystoneFramework.KeyDebug
 {
     public static class KeyTester
     {
-        public static void Log<T>(string message, Color color = default)
-        { 
+        public static void Log(string message, Color color = default)
+        {
+            if (color == default) color = Color.white;
+            var stackTrace = new StackTrace();
+            var method = stackTrace.GetFrame(1).GetMethod();
+            string className = method.DeclaringType != null ? method.DeclaringType.FullName : "Not Found";
             string hexColor = ColorUtility.ToHtmlStringRGBA(color);
-            StringBuilder builder = new StringBuilder();
-            builder.Append($"<color=#{hexColor}>");
-            builder.Append($"{typeof(T).Name}: {message})");
-            builder.Append($"</color>");
-            Debug.Log(builder.ToString());
-        }
+            Debug.Log($"<color=cyan>{className}</color> : <color=#{hexColor}>{message}</color>");
+        } 
     }
 }
