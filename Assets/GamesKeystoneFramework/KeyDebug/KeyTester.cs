501c2ecee58bf390ccf03a09774bf355616c4be5
TaguchiRei <ren.len2426@gmail.com>
2025-05-30 10:31:29 +0900

    [update]名前変更

--- Assets/GamesKeystoneFramework/KeyDebug/KeyTester.cs
+++ Assets/GamesKeystoneFramework/KeyDebug/KeyTester.cs
@@ -3,14 +3,14 @@ using UnityEngine;
 
 namespace GamesKeystoneFramework.KeyDebug
 {
-    public static class KeyDebug
+    public static class KeyTester
     {
         public static void Log<T>(string message, Color color = default)
         { 
             string hexColor = ColorUtility.ToHtmlStringRGBA(color);
             StringBuilder builder = new StringBuilder();
             builder.Append($"<color=#{hexColor}>");
-            builder.Append($"{nameof(T)}: {message})");
+            builder.Append($"{typeof(T).Name}: {message})");
             builder.Append($"</color>");
             Debug.Log(builder.ToString());
         }
